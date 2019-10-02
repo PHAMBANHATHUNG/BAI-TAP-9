6e5f4c05759ed22f568446c1944b4d972a2a0373
PHAMBANHATHUNG <hunggaw3@gmail>
2019-10-02 13:39:05 +0700

    Add project files.

---
+++ AOSP/Student.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+namespace AppOOP
+{
+    class Student
+    {
+        public Student()
+        {
+
+        }
+
+        public Student(string name)
+        {
+            this.name = name;
+        }
+
+        private string name;
+
+        public string StudentName { get => name; set => name = value; }
+
+        public void Talk()
+        {
+            MessageBox.Show(StudentName);
+        }
+    }
+}
